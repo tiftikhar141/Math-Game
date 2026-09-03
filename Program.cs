06153b0fe1f5ad5c011c9dc308e0de0b4207b116
tiftikhar141 <tiftikhar775@gmail.com>
2026-09-03 16:37:14 -0500

    added support for calculation options, reordered menu and calculation method

--- Program.cs
+++ Program.cs
@@ -4,11 +4,49 @@
     {
         static void Main(string[] args)
         {
-            //menu();
-            //calculations(1);
+            menu();
+        }
+        static void menu()
+        {
+            int menuOption;
+            do
+            {
+                Console.WriteLine("Choose your operation:\n1) Addition, 2) Subtraction, 3) Multiplication, 4) Division");
+                menuOption = Convert.ToInt32(Console.ReadLine());
+
+                if (menuOption < 1 || menuOption > 4)
+                {
+                    Console.WriteLine("Inavlid option, please try again.");
+                }
+
+            } while (menuOption < 1 || menuOption > 5);
+
+            if (menuOption == 1)
+            {
+                calculations(1);
+            }
+            if (menuOption == 2)
+            {
+                calculations(2);
+            }
+            if (menuOption == 3)
+            {
+                calculations(3);
+            }
+            if (menuOption == 4)
+            {
+                calculations(4);
+            }
+
+        }
+        static void calculations(int operation)
+        {
 
             // print out 5 math questions
             var random = new Random();
+            int userAnswer;
+            int correctAnswer = 0;
+            string operationSign = ",";
 
             for (int i = 0; i < 5; i++)
             {
@@ -20,69 +58,40 @@
                 int multiplicationAnswer = number1 * number2;
                 int divisionAnswer = number1 / number2;
 
-                int userAnswer;
+                if (operation == 1)
+                {
+                    operationSign = "+";
+                    correctAnswer = additionAnswer;
+                }
+                if (operation == 2)
+                {
+                    operationSign = "-";
+                    correctAnswer = subtractionAnswer;
+                }
+                if (operation == 3)
+                {
+                    operationSign = "x";
+                    correctAnswer = multiplicationAnswer;
+                }
+                if (operation == 4)
+                {
+                    operationSign = "÷";
+                    correctAnswer = divisionAnswer;
+                }
 
-                Console.WriteLine(number1 + " + " + number2);
+                Console.WriteLine(number1 + operationSign + number2);
                 userAnswer = Convert.ToInt32(Console.ReadLine());
 
-                if (userAnswer == additionAnswer) {
-                    Console.WriteLine("correct");
-                } else {
-                    Console.WriteLine("wrong");
-                }
-
-            }
-            
-
-
-        }
-
-        static int calculations(int operation)
-        {
-            var random = new Random();
-            int number = (int)random.NextInt64(0, 100);
-
-
-
-            return 0;
-
-        }
-
-        static void menu()
-        {
-            int menuOption;
-            do
-            {
-                Console.WriteLine("Choose your operation:\n1) Addition, 2) Subtraction, 3) Multiplication, 4) Division, 5) Previous Results");
-                menuOption = Convert.ToInt32(Console.ReadLine());
-
-                if (menuOption < 1 || menuOption > 5)
+                if (userAnswer == correctAnswer)
                 {
-                    Console.WriteLine("Inavlid option, please try again.");
+                    Console.WriteLine("Correct!");
+                } else
+                {
+                    Console.WriteLine("Wrong");
                 }
-
-            } while (menuOption < 1 || menuOption > 5);
-
-            if (menuOption == 1)
-            {
-                Console.WriteLine("Addition");
-            }
-            if (menuOption == 2)
-            {
-                Console.WriteLine("Subtraction");
-            }
-            if (menuOption == 3)
-            {
-                Console.WriteLine("Multiplication");
-            }
-            if (menuOption == 4)
-            {
-                Console.WriteLine("Division");
-            }
-            if (menuOption == 5)
-            {
-                Console.WriteLine("Past Results");
             }
         }
+
+        
     }
 }
